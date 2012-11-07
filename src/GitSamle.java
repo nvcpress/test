c2953263daf5d098f43a950bc3d6fc79b32fd81b
kaldente <jay4kyung@gmail.com>
2012-11-06 20:30:23 -0800

    added subtractTwoNumber method

--- src/GitSamle.java
+++ src/GitSamle.java
@@ -1,7 +1,11 @@
 
 public class GitSamle {
 	public int sum;
+	public int subtraction;
 	public void addTwoNumber(int one, int two){
 		this.sum = one + two;
+	}
+	public void subtractTwoNumber(int one, int two){
+		this.subtraction = one - two;
 	}
 }
