0a6ac2ef1501515af61bb07080097da48bd27d2c
kaldente <jay4kyung@gmail.com>
2012-11-07 00:19:35 -0800

    checking in all the report html files

--- src/org/jaykyung/test/TestGitSample.java
+++ src/org/jaykyung/test/TestGitSample.java
@@ -1,15 +1,8 @@
 package org.jaykyung.test;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
 import org.junit.Test;
 
 public class TestGitSample {
-
-	@After
-	public void tearDown() throws Exception {
-	}
 
 	@Test
 	public void test() {
